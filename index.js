b636d990d82475f84253ea3207e4c96efba8f8d0
tamanna <tamannabegum646@gmail.com>
2025-04-09 19:13:12 +0200

    comment

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. Write a function called capitalize() that takes a string and uses.
+// map to return the same string in all caps.
+
+
+const capitalize = (str => str)
+capitalize.map(char => char.toUpperCase())
+console.log('aadsdsddf');
